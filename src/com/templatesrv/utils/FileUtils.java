e60b95198a00acd5550a190881c3f45c80cfd622
Luke Donovan <ladbuilder1@gmail.com>
2017-07-30 16:26:32 -0400

    removed unused xml method

--- src/com/templatesrv/utils/FileUtils.java
+++ src/com/templatesrv/utils/FileUtils.java
@@ -1,14 +1,8 @@
 package com.templatesrv.utils;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
 
 import com.google.gson.Gson;
 
@@ -23,24 +17,10 @@ public class FileUtils {
 		}
 	}
 
-	public static Document readXML(String fileName) {
-		try {
-			File fXmlFile = new File(fileName);
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
-			doc.getDocumentElement().normalize();
-			return doc;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
 	public static JSONResponseSettings readJSONSettings(String fileName) {
 		return new Gson().fromJson(readFile(fileName), JSONResponseSettings.class);
 	}
-	
+
 	public static JSONResponseURLs readJSONURLs(String fileName) {
 		return new Gson().fromJson(readFile(fileName), JSONResponseURLs.class);
 	}
